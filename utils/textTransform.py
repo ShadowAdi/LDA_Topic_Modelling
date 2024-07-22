da3d709989e9b69d3869be856879a70837cf29a7
Aditya Shukla <Shadowshukla76@gmail.com>
2024-07-22 11:52:58 +0530

    basic

--- utils/textTransform.py
+++ utils/textTransform.py
@@ -1,11 +1,24 @@
-from Basic import basic_filter
 import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+nlp = spacy.load('en_core_web_sm',disable=["parser","ner"])
+nlp.Defaults.stop_words.add("say")
 from gensim import models
 lda_model = models.LdaModel.load('models\lda_model.gensim')
 research_model_lda=models.LdaModel.load("models\lda_model_research_articles.gensim")
 dictinary_research=research_model_lda.id2word
 dictionary = lda_model.id2word
 
+
+
+
+
+
+def basic_filter(tokenized_doc):
+    return [t.lemma_ for t in tokenized_doc if t.is_alpha and t.text not in STOP_WORDS and t.pos_ in ["NOUN", "VERB", "ADJ"]]
+
+
+
+    
 
 
 nlp = spacy.load('en_core_web_sm',disable=["parser","ner"])
